053b921a18bb520a0d507cd23f61f6e77832d914
moinmulla7045-lab <moinmulla7045@gmail.com>
2026-03-09 15:06:26 +0530

    Add main App component as the starting point for the Ludo King game.

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="app">
+      <h1 className="text-3xl font-bold text-center">Ludo King Game</h1>
+      <p className="text-center">Game structure is initializing...</p>
+    </div>
+  );
+};
+
+export default App;
